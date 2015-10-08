643ecd4cb246d6240641b5576254f85cf9e6b59a
Veske <veske@tlu.ee>
2015-10-08 20:16:18 +0300

    Initial maze completion

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 __author__ = 'Martin, Kaspar'
 
-import lab2
+import random
+
+from lab2 import SearchProblem, Fringe
 
 
-## Tsopic's Cave
-p = lab2.SearchProblem(131316);
+def silly_search(problem):
+    fringe = Fringe()
+    fringe.add_front(problem.start_node())
 
-p.dump();
+    while 1:  # lõputu tsükkel
+        if fringe.is_empty():
+            return None  # ei leitud lahendust
 
+        node = fringe.remove_front()
+        if problem.is_goal(node):
+            return node  # puu tipp. Tee siit puu juurikasse ongi lahendus
+
+        children = problem.expand(node)  # children on list tyypi; [ node1, node2, .... ]
+        fringe.add_front(random.choice(children))  # valime yhe ja paneme jrk ette
+
+
+p = SearchProblem(131333)
+res = silly_search(p)
+if res is None:
+    print("Not found")
+else:
+    p.print_path(res)
+    p.print_solution(res)
